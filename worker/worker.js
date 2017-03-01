639dd1718dac82a84ffd1371000ae9dc5b68119e
jackyr <jackyrao2009@gmail.com>
2017-03-01 15:30:58 +0800

    update

--- worker/worker.js
+++ worker/worker.js
@@ -1,6 +1,6 @@
 // console.log('worker init');
 console.log(this, self);
-onmessage = function (event) {
+onmessage = function (event) {event.data.a = 1;
   console.log("worker onmessage:", event.data);
 
   var now = Date.now();
