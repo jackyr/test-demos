80611084da5b0f1df047649b6fd9c426c2060dfb
jackyr <jackyrao2009@gmail.com>
2017-03-06 00:40:25 +0800

    update

--- worker/worker.js
+++ worker/worker.js
@@ -1,6 +1,6 @@
 // console.log('worker init');
 onmessage = function (event) {
-  console.log("worker onmessage:", event.data);
+  console.log("worker onmessage:", event.data, 2);
 
   var now = Date.now();
   while (Date.now() - now <= 2000) {}
