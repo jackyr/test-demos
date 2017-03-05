e47a699a168e771c99c6af384bebef80cd5e5386
jackyr <jackyrao2009@gmail.com>
2017-03-05 22:55:22 +0800

    update

--- shared-worker/worker.js
+++ shared-worker/worker.js
@@ -5,7 +5,7 @@ onconnect = function (event) {
   port = event.ports[0];
   console.log("worker onconnect:", 'A new connection! The current connection number is ' + ++num);
   port.onmessage = function (event) {
-    alert(1);
+    console.log(port.close);
     console.log("worker onmessage:", event.data);
     port.postMessage(event.data);
     port.close();
