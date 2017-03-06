8ca13ea83422b171adbe2fec008a5f4de9a26a7e
jackyr <jackyrao2009@gmail.com>
2017-03-06 18:54:19 +0800

    update

--- shared-worker/worker.js
+++ shared-worker/worker.js
@@ -1,12 +1,22 @@
-var port = null;
-var num = 0;
-onconnect = function (event) {
-  port = event.ports[0];
-  console.log("worker onconnect:", 'A new connection! The current connection number is ' + ++num);
-  port.onmessage = function (event) {
-    console.log("worker onmessage:", event.data + '1');
-    port.postMessage(event.data);
-    // close();
-  };
+var i = 0;
+var portSet = [];
+var numSet = [];
+onconnect = event => {
+  var port = event.ports[0];
+  portSet.push(port);
+  numSet.push(0);
+  console.log("worker onconnect:", 'A new connection! The current connection number is ' + ++i);
+  
+  port.onmessage = (i => event => {
+    var data = event.data;
+    numSet[i-1] = data - 0;
+    portSet.forEach(port => {
+      port.postMessage(getResult(numSet));
+    });
+  })(i);
   // port.start();
+
+  function getResult(set) {
+    return set.length > 1 ? `${set.join(' + ')} = ${set.reduce((prev, curr) => prev + curr)}` : set[0];
+  }
 };
