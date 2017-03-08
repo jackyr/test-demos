690fec490042b8be50fec3c1d967e77ba900705a
jackyr <jackyrao2009@gmail.com>
2017-03-08 10:59:59 +0800

    update

--- worker/worker.js
+++ worker/worker.js
@@ -1,10 +1,11 @@
 // console.log('worker init');
+importScripts('common.js');
 onmessage = function (event) {
   console.log("worker onmessage:", event.data);
 
   var now = Date.now();
   while (Date.now() - now <= 2000) {}
   
-  postMessage(event.data);
+  postMessage(event.data + ' ' + name);
   close();
 };
