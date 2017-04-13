63e5e57d497ce162a222e40d5c4b6f3bfb300f7e
jackyr <jackyrao2009@gmail.com>
2017-04-13 16:21:03 +0800

    update

--- service-worker/worker.js
+++ service-worker/worker.js
@@ -1,43 +1,48 @@
+const CACHE_LIST = [
+  'https://os.alipayobjects.com/rmsportal/hVsghLyuoDyZovLGhSxl.json',
+  //'/test-demos/service-worker/',
+  '/test-demos/service-worker/offline.html',
+  '/test-demos/service-worker/img/1.jpeg',
+];
+
 self.addEventListener('install', function(event) {
-  /*event.waitUntil(
+  event.waitUntil(
     caches.open('v1').then(function(cache) {
-      return cache.addAll([
-        'https://os.alipayobjects.com/rmsportal/hVsghLyuoDyZovLGhSxl.json',
-        //'/test-demos/service-worker/',
-        '/test-demos/service-worker/img/1.jpeg',
-      ]);
+      return cache.addAll(CACHE_LIST);
     })
-  );*/
+  );
 });
 
 self.addEventListener('fetch', function(event) {
   console.log(event.request.url);
-  event.respondWith(
-    caches
-    .match(event.request)
-    .catch(() => {
-      console.log(1);
-      return fetch(event.request);
-    })
-    .then(response => {
-      console.log(2， response);
-      if (response) {
-        return response;
-      }
-      return fetch(event.request)
-        .then(fetchResponse => {
-          //caches.open('v1').then(cache => {
-            //cache.put(event.request, response);
-          //});
-          return fetchResponse;
-        });      
-    })
-    .catch(() => {
-      console.log(3);
-      return new Response('11111', {status: 200, contentType: ''});
-    })
-  );
-  // event.respondWith(
-    // caches.match(event.request);
-  // );
+  //if (CACHE_LIST.find(item => event.request.url.endWith(item))) {
+    event.respondWith(
+      caches
+      .match(event.request)
+      .catch(() => {
+        console.log(1);
+        return fetch(event.request);
+      })
+      .then(response => {
+        console.log(2, response);
+        if (response) {
+          return response;
+        }
+        return fetch(event.request)
+          .then(fetchResponse => {
+            //caches.open('v1').then(cache => {
+              //cache.put(event.request, response);
+            //});
+            return fetchResponse;
+          });      
+      })
+      .catch(() => {
+        console.log(3);
+        return caches.match('/test-demos/service-worker/offline.html');
+      })
+    );
+    // event.respondWith(
+      // caches.match(event.request);
+    // );
+  //}
 });
